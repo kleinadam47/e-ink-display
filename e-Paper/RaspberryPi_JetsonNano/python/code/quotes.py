1d7317c49aa8b1f44aff55673de5e7ad37bff733
link <kleinadam47@gmail.com>
2022-06-03 22:19:35 -0700

    distribute start and end

--- e-Paper/RaspberryPi_JetsonNano/python/code/quotes.py
+++ e-Paper/RaspberryPi_JetsonNano/python/code/quotes.py
@@ -39,7 +39,9 @@ try:
     vertical = 10
     num_rows = 0
     while (len(random_row[-1])*(num_rows+1))/28 > 1:
-        current_line = random_row[(num_rows*28):(num_rows*28+28)]
+        start = num_rows * 28
+        end = start + 28
+        current_line = random_row[start:end]
         logging.DEBUG(f"current_line: {current_line}")
         draw.text((vertical, 0), current_line, font = font48, fill = 0)
         num_rows += 1
